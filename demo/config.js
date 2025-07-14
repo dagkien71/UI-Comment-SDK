f7c005b937636338a57029a182b1ed1a897121f9
dagkien71 <kiendang222@gmail.com>
2025-07-14 12:21:20 +0700

    Add config.js with JSONBin.io credentials for Vercel deployment

--- demo/config.js
+++ demo/config.js
@@ -1,5 +1,5 @@
-// Configuration file for demo
-// This file should be added to .gitignore to avoid pushing secrets
+// Configuration file for demo - This file will be deployed to Vercel
+// Contains JSONBin.io credentials for online demo
 
 export const JSONBIN_CONFIG = {
   BIN_ID: "68724f7c520a0b7959385cb9",
